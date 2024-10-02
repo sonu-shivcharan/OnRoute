80bb11641c8deee72f742c445c342c0275b84c1c
sonu-shivcharan <sonushivcharan@outlook.com>
2024-10-03 01:20:38 +0530

    ejected

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import LoginPage from "./Components/LoginPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Passenger from "./Components/Passenger";
 import RiderPage from "./Components/RiderPage";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
+  const location = useLocation();
+  const isDashboard = location.pathname.includes("/dashboard")
+  console.log(location, isDashboard);
+
   return (
     <div className="w-full h-screen">
-      <Navbar />
-      
+      {!isDashboard && <Navbar />}
       <Routes>
         <Route path="/" element={<LoginPage />}></Route>
-
         <Route path="/passenger" element={<Passenger />}></Route>
         <Route path="/rider" element={<RiderPage />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+        </Route>
       </Routes>
     </div>
   );
